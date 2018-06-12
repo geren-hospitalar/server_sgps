1d9e259cfd89312af20207f645f573c4a1aebbfe
julio_cubano <programas.trabalho20@gmail.com>
2018-06-12 09:42:07 -0400

    first commit

---
+++ serverSgps/src/main/java/br/com/sgps/serverSgps/ServerSgpsApplication.java
@@ -0,0 +1,12 @@
+package br.com.sgps.serverSgps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSgpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerSgpsApplication.class, args);
+	}
+}
---
+++ serverSgps/src/test/java/br/com/sgps/serverSgps/ServerSgpsApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.sgps.serverSgps;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServerSgpsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
